21b7c7934079d4aaab35ec5271481c8e16ba0400
Mindaugas Žiemys <mindaugas.ziemys@4finance.com>
2026-03-19 22:30:07 +0200

    add data analysis

---
+++ config.py
@@ -0,0 +1,8 @@
+raw_csv = 'data/1_raw'
+rows_to_skip = 17
+start_year = 2012
+end_year = 2025
+features = ['CO', 'O3', 'NO', 'NO2', 'PM10', 'PM2.5', 'wind_speed', 'temp', 'wind_dir_sin', 'wind_dir_cos']
+pollutants = ['CO', 'O3', 'NO', 'NO2', 'PM10', 'PM2.5']
+scaled_csv = 'data/3_scaled/scaled.csv'
+unscaled_csv = 'data/2_unscaled/unscaled.csv'
